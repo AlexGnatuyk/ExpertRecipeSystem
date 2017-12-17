04865a3b82a24cd7b2844fb52cdad818c8f71af4
Aleksandr <Alex-Gnatuyk@yandex.ru>
2017-12-17 14:49:10 +0300

    Add files

--- ExpertRecipeSystem/Program.cs
+++ ExpertRecipeSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace ExpertRecipeSystem
     {
         static void Main(string[] args)
         {
+
+            var system = new CSVExpertSystem("Таблица переходов.csv", "Таблица вопросов.csv");
+            var endOf = false;
+            var result = system.NextQuestion("", out endOf);
+            while (true)
+            {
+
+                if (result != null)
+                {
+                    Console.WriteLine(result);
+                    result = system.NextQuestion(Console.ReadLine(), out endOf);
+                }
+                else
+                {
+                    Console.WriteLine("End");
+                    break;
+                }
+            }
+            Console.Read();
         }
     }
 }
